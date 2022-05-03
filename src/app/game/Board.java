5b0a77fe197bf8568c8fd1e049368cf146b4eb5c
han-tu <haann.m5@gmail.com>
2022-05-03 12:51:12 +0700

    add: Board Object

--- src/app/game/Board.java
+++ src/app/game/Board.java
@@ -5,4 +5,47 @@ import java.util.Hashtable;
 public class Board {
 	private Hashtable <String, String> boardMap;
 	
+	/*
+	 * Ship Count:
+	 * Small: 1
+	 * Medium-Small : 2
+	 * Medium-Large : 1
+	 * Large : 1
+	 */
+	
+	public Board() {
+		this.boardMap = new Hashtable<String, String>();
+		initBoardMap();
+	}
+	
+	private void initBoardMap() {
+		for (int i = 65 ; i <= 70 ; i++) {
+			char vertical = (char) i;
+			for (int j = 1 ; j <= 10 ; j++) {
+				String pos = vertical + Integer.toString(j);
+				this.boardMap.put(pos, "U");
+			}
+		}
+	}
+	
+	public void setBoardConditionAt(String pos, String cond) {
+		this.boardMap.replace(pos, cond);
+	}
+	
+	public String toString() {
+		String board = "- 1 2 3 4 5 6 7 8 9 10\n";
+		for (int i = 65 ; i <= 70 ; i++) {
+			char vertical = (char) i;
+			for (int j = 1 ; j <= 10 ; j++) {
+				String pos = vertical + Integer.toString(j);
+				if (j == 1)
+					board += vertical;
+				board += " ";
+				board += this.boardMap.get(pos);
+				
+			}
+			board += "\n";
+		}
+		return board;
+	}
 }
--- src/app/testing/TestClass.java
+++ src/app/testing/TestClass.java
@@ -1,17 +1,24 @@
 package app.testing;
 
 import app.exceptions.ShipNotValidException;
+import app.game.Board;
 import app.game.Ship;
 
 public class TestClass {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		try {
-			Ship myShip = new Ship(2, "A2", "B3");
-		} catch (ShipNotValidException e) {
-			System.out.println(e.getMessage());
-		}
+//		try {
+//			Ship myShip = new Ship(2, "A2", "B3");
+//		} catch (ShipNotValidException e) {
+//			System.out.println(e.getMessage());
+//		}
+		Board board = new Board();
+		board.setBoardConditionAt("A5", "H");
+		board.setBoardConditionAt("B5", "H");
+		board.setBoardConditionAt("C5", "H");
+		board.setBoardConditionAt("A6", "M");
+		System.out.println(board.toString());
 		System.out.println("end");
 	}
 
