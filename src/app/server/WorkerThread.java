44a30f87abc7da0183f4910d7e057f637dbef5b5
SalmanDA <alfariqsalman@gmail.com>
2022-06-06 22:08:21 +0700

    room handling

--- src/app/server/WorkerThread.java
+++ src/app/server/WorkerThread.java
@@ -9,27 +9,27 @@ import app.game.Message;
 
 public class WorkerThread extends Thread {
 	private Socket socket;
-    private ObjectOutputStream ous;
-    private ObjectInputStream ois;
-    private RoomThread rt;
-    private String username;
-    private boolean ready;
-    private boolean gameOver;
-    
-    public WorkerThread(Socket socket, RoomThread roomThread) {
-        try {
-            this.socket = socket;
-            this.ous = new ObjectOutputStream(this.socket.getOutputStream());
-            this.ois = new ObjectInputStream(this.socket.getInputStream());
-            this.rt = roomThread;
-            this.setGameOver(false);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
-    
-    public void run() {
-    	try {
+	private ObjectOutputStream ous;
+	private ObjectInputStream ois;
+	private RoomThread rt;
+	private String username;
+	private boolean ready;
+	private boolean gameOver;
+
+	public WorkerThread(Socket socket, RoomThread roomThread) {
+		try {
+			this.socket = socket;
+			this.ous = new ObjectOutputStream(this.socket.getOutputStream());
+			this.ois = new ObjectInputStream(this.socket.getInputStream());
+			this.rt = roomThread;
+			this.setGameOver(false);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	public void run() {
+		try {
 			Message firstMessage = (Message) this.ois.readObject();
 			this.setUsername(firstMessage.getText());
 			rt.createPlayer(this);
@@ -38,72 +38,71 @@ public class WorkerThread extends Thread {
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
-    	
-    	while(!this.isGameOver()) {
-    		try {
+
+		while (!this.isGameOver()) {
+			try {
 				Message message = (Message) this.ois.readObject();
-				
-				
+
 				// Handling Room yang belum penuh
-				
-				/*
-				 * Code Here
-				 */
-				
-				String reqType = message.getRequest().split(" ")[0];
-				if (reqType.equals("send-message")) {
-					Message newMessage = rt.createMessage(message.getRequest().split(" ")[1], this.username, rt.getOpponentName(this.username));
-					newMessage.setRequest("Message");
+
+				if (rt.getPlayerCount() == 2) {
+
+					String reqType = message.getRequest().split(" ")[0];
+					if (reqType.equals("send-message")) {
+						Message newMessage = rt.createMessage(message.getRequest().split(" ")[1], this.username,
+								rt.getOpponentName(this.username));
+						newMessage.setRequest("Message");
+						rt.sendMessage(newMessage);
+					} else if (reqType.equals("see")) {
+						if (message.getRequest().split(" ")[1].equals("-mb")) {
+							String board = rt.getBoard(this.username).toString();
+							Message newMessage = rt.createMessage(board, "Server", this.username);
+							newMessage.setRequest("");
+							rt.sendMessage(newMessage);
+						} else if (message.getRequest().split(" ")[1].equals("-ob")) {
+							String board = rt.getBoard(rt.getOpponentName(this.username)).toString();
+							Message newMessage = rt.createMessage(board, "Server", this.username);
+							newMessage.setRequest("");
+							rt.sendMessage(newMessage);
+						}
+					} else if (reqType.equals("fire")) {
+						String tile = message.getRequest().split(" ")[1];
+						rt.attack(this.username, tile);
+					}
+				} else {
+					Message newMessage = rt.createMessage("Waiting for opponent", "Server", this.username);
+					newMessage.setRequest("");
 					rt.sendMessage(newMessage);
 				}
-				else if (reqType.equals("see")) {
-					if (message.getRequest().split(" ")[1].equals("-mb")) {
-						String board = rt.getBoard(this.username).toString();
-						Message newMessage = rt.createMessage(board, "Server", this.username);
-						newMessage.setRequest("");
-						rt.sendMessage(newMessage);
-					}
-					else if (message.getRequest().split(" ")[1].equals("-ob")) {
-						String board = rt.getBoard(rt.getOpponentName(this.username)).toString();
-						Message newMessage = rt.createMessage(board, "Server", this.username);
-						newMessage.setRequest("");
-						rt.sendMessage(newMessage);
-					}
-				}
-				else if (reqType.equals("fire")) {
-					String tile = message.getRequest().split(" ")[1];
-					rt.attack(this.username, tile);
-				}
-				
 			} catch (ClassNotFoundException e) {
 				e.printStackTrace();
 			} catch (IOException e) {
 				e.printStackTrace();
 			}
-    		
-    	}
-    }
-    
-    public void send(Message message) {
-        try {
-            this.ous.writeObject(message);
-            this.ous.flush();
-        } catch (IOException e) {
-            // Code Here
-        }
-    }
-    
-    public String getUsername() {
-    	return this.username;
-    }
-    
-    public void setUsername(String username) {
-    	this.username = username;
-    }
-    
-    public Socket getSocket() {
-    	return this.socket;
-    }
+
+		}
+	}
+
+	public void send(Message message) {
+		try {
+			this.ous.writeObject(message);
+			this.ous.flush();
+		} catch (IOException e) {
+			// Code Here
+		}
+	}
+
+	public String getUsername() {
+		return this.username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public Socket getSocket() {
+		return this.socket;
+	}
 
 	public boolean isGameOver() {
 		return gameOver;
